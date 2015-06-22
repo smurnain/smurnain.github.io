c0041209c20f4ff0ecf6725584b2fff792f062ac
Stuart Murnain <stu@murnain.id.au>
2015-06-22 20:32:08 +1000

    added .gitignore

--- _site/build_blog.R
+++
@@ -1,9 +0,0 @@
-# Create a simple blog using bootstrap blog template
-# Author: Stuart Murnain
-
-library(R2HTML)
-setwd("/home/stu//github/smurnain.github.io/")
-
-blog <- HTMLInitFile(outdir=getwd(),filename="Test")
-HTML(as.title("Test page"),blog)
-HTMLEndFile()
